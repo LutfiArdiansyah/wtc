4d3c5ffae69de258ff7b4956800d36dff5243496
Lutfi Ardiansyah <lutfi@kct.co.id>
2021-12-14 01:17:03 +0700

    config db

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', '103.174.115.41'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi-dev-368'),
+        database: env('DATABASE_NAME', 'strapi-dev'),
         username: env('DATABASE_USERNAME', 'root'),
         password: env('DATABASE_PASSWORD', 'Gbi@2021'),
         ssl: env.bool('DATABASE_SSL', false),

03ce50ffa545a5c167f778f4bd5df5c9fe059308
Aziz <abdul.aziz@muf.co.id>
2021-12-18 11:08:28 +0700

    Penyesuaian base url.

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://103.174.115.41:1337',
+  url: 'http://wtc.a-aziz.tech:1337',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'a277c0a2593ff36435eb068a3e89d66f'),
